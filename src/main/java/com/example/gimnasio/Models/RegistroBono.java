4b7eb4e19f553eb2ce9fa4e15ba18f38037cd7a1
Victor Sanchez <mastetnono@gmail.com>
2026-05-07 12:33:49 +0200

    update: registroBono

--- src/main/java/com/example/gimnasio/Models/RegistroBono.java
+++ src/main/java/com/example/gimnasio/Models/RegistroBono.java
@@ -26,4 +26,51 @@ public class RegistroBono {
     @Column(nullable = false)
     private int usosDos;
 
+    public int getIdBono() {
+        return idBono;
+    }
+
+    public void setIdBono(int idBono) {
+        this.idBono = idBono;
+    }
+
+    public Usuario getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Usuario idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getBonosUno() {
+        return bonosUno;
+    }
+
+    public void setBonosUno(int bonosUno) {
+        this.bonosUno = bonosUno;
+    }
+
+    public int getBonosDos() {
+        return bonosDos;
+    }
+
+    public void setBonosDos(int bonosDos) {
+        this.bonosDos = bonosDos;
+    }
+
+    public int getUsosUno() {
+        return usosUno;
+    }
+
+    public void setUsosUno(int usosUno) {
+        this.usosUno = usosUno;
+    }
+
+    public int getUsosDos() {
+        return usosDos;
+    }
+
+    public void setUsosDos(int usosDos) {
+        this.usosDos = usosDos;
+    }
 }
