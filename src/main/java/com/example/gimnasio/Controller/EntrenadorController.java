bdfbd8cb9d22dc06cc7d93344c5ecebc5b2b0db7
rafaTorre <tardaranbuo@gmail.com>
2026-05-14 11:53:41 +0200

    add: logueo

---
+++ src/main/java/com/example/gimnasio/Controller/EntrenadorController.java
@@ -0,0 +1,9 @@
+package com.example.gimnasio.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/entrenador")
+public class EntrenadorController {
+}
