d8303c20f7bd8083acd05823bfd0375783e8cb2c
rafaTorre <tardaranbuo@gmail.com>
2026-05-14 10:23:57 +0200

    Creacion clases

---
+++ src/main/java/com/example/gimnasio/Models/RegistroClase.java
@@ -0,0 +1,9 @@
+package com.example.gimnasio.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Table(name="RegistroClases")
+@Entity
+public class RegistroClase {
+}
---
+++ src/main/java/com/example/gimnasio/Models/RegistroUsuarioBono.java
@@ -0,0 +1,9 @@
+package com.example.gimnasio.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Table
+@Entity
+public class RegistroUsuarioBono {
+}
