7733e0721c181991a4f3d30b697f2d52433d9c40
rafaTorre <tardaranbuo@gmail.com>
2026-05-14 10:48:13 +0200

    add: modelos

--- src/main/java/com/example/gimnasio/Models/Clase.java
+++ src/main/java/com/example/gimnasio/Models/Clase.java
@@ -1,9 +1,70 @@
 package com.example.gimnasio.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Table
 @Entity
 public class Clase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_clase")
+    private int idCalse ;
+
+    @Column(length = 50 , nullable = false)
+    private LocalDate fecha ;
+
+    @Column(length = 50 , nullable = false)
+    private LocalTime hora ;
+
+    @Column(name ="cupo_max" ,nullable = false)
+    private int cupoMax ;
+
+    @ManyToOne
+    @JoinColumn(name = "id_entrenador")
+    private Entrenador entrenador ;
+
+
+    public int getIdCalse() {
+        return idCalse;
+    }
+
+    public void setIdCalse(int idCalse) {
+        this.idCalse = idCalse;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public int getCupoMax() {
+        return cupoMax;
+    }
+
+    public void setCupoMax(int cupoMax) {
+        this.cupoMax = cupoMax;
+    }
+
+    public Entrenador getEntrenador() {
+        return entrenador;
+    }
+
+    public void setEntrenador(Entrenador entrenador) {
+        this.entrenador = entrenador;
+    }
 }
--- src/main/java/com/example/gimnasio/Models/TipoBono.java
+++ src/main/java/com/example/gimnasio/Models/TipoBono.java
@@ -1,9 +1,41 @@
 package com.example.gimnasio.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Table
 @Entity
 public class TipoBono {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idBono ;
+
+    @Column(length = 50 , nullable = false)
+    private String tituloBono ;
+
+    @Column(nullable = false)
+    private int numeroDeUsos ;
+
+    public int getIdBono() {
+        return idBono;
+    }
+
+    public void setIdBono(int idBono) {
+        this.idBono = idBono;
+    }
+
+    public String getTituloBono() {
+        return tituloBono;
+    }
+
+    public void setTituloBono(String tituloBono) {
+        this.tituloBono = tituloBono;
+    }
+
+    public int getNumeroDeUsos() {
+        return numeroDeUsos;
+    }
+
+    public void setNumeroDeUsos(int numeroDeUsos) {
+        this.numeroDeUsos = numeroDeUsos;
+    }
 }
