5a45c43bf77ee41fbdfa424e9089db7adf32058b
rafaTorre <tardaranbuo@gmail.com>
2026-05-14 13:11:59 +0200

    add: logueo 3

--- src/main/java/com/example/gimnasio/Repository/EntrenadorRepository.java
+++ src/main/java/com/example/gimnasio/Repository/EntrenadorRepository.java
@@ -1,7 +1,11 @@
 package com.example.gimnasio.Repository;
 
+import com.example.gimnasio.DTO.EntrenadorDTO;
 import com.example.gimnasio.Models.Entrenador;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EntrenadorRepository extends JpaRepository<Entrenador, Integer> {
+    Optional<EntrenadorDTO> findByEmailAndPassword(String email , String password);
 }
