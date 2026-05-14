21045105e4e4718fc59da8d9f6c4874da25c08be
rafaTorre <tardaranbuo@gmail.com>
2026-05-14 11:49:21 +0200

    fix : pagina principal

---
+++ src/main/java/com/example/gimnasio/Controller/PrincipalController.java
@@ -0,0 +1,12 @@
+package com.example.gimnasio.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PrincipalController {
+    @GetMapping("")
+    public String iniciar(){
+        return "Principal.html";
+    }
+}
--- src/main/java/com/example/gimnasio/Models/RegistroClase.java
+++ src/main/java/com/example/gimnasio/Models/RegistroClase.java
@@ -1,6 +1,8 @@
 package com.example.gimnasio.Models;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -22,6 +24,7 @@ public class RegistroClase {
     private Clase clase ;
 
     @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.VARCHAR)
     @Column(name = "estado", length = 15, nullable = false)
     private Estado estado;
 
